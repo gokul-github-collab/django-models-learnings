131eb1a577d87b4db406042ad113e10b65fcf154
gokul-github-collab <gokulakrishnan0604@gmail.com>
2023-07-04 19:17:33 +0530

    Models Completed

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def index(request):
-    pass
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {"books": books})
+
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/book_detail.html", {"book": book})
